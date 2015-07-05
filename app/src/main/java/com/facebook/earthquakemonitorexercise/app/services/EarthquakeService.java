9f2e28bbb72e0b85da67684ddae6fe1ebb64da14
Nidhi Shah <nidhishah@nidhishah-mbp.local>
2015-07-04 21:32:16 -0700

    first commit

--- app/src/main/java/com/facebook/earthquakemonitorexercise/app/services/EarthquakeService.java
+++ app/src/main/java/com/facebook/earthquakemonitorexercise/app/services/EarthquakeService.java
@@ -19,5 +19,8 @@ public class EarthquakeService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         // TODO: Get json from the  earthquake data returned by the url
         // TODO: Construct an intent and fire a broadcast to send the json string to the activity.
+        // 1. Construct an Intent tying it to the ACTION (arbitrary event namespace)
+        // 2. Put extras KEY_RESULTS, KEY_RESULT_CODE into the intent as usual
+        // 3. Fire the broadcast with intent packaged
     }
 }
